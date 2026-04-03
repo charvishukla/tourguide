41c2bd2e4f5dd6402d9f59d2030a27c5abea8e82
Charvi Shukla <cshukla@ucsd.edu>
2026-04-03 21:17:46 +0200

    [Waypoint Follower] clean and refactor scripts

--- traverse_waypoints.py
+++ traverse_waypoints.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python3
 
+
 import math
 import rclpy
+import yaml
 from geometry_msgs.msg import PoseStamped
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 
 
-def make_pose(navigator, x, y, yaw_deg=0.0, frame_id='map'):
-    pose = PoseStamped()
-    pose.header.frame_id = frame_id
-    pose.header.stamp = navigator.get_clock().now().to_msg()
+FILE_PATH = 'Desktop/tourguide/waypoints.yaml'
 
+def make_pose(navigator, x, y, yaw_deg=0.0, frame_id='map'):
+    '''
+    This function can be used to specify an orientation and position for the robot. 
+    Inputs:
+        - navigator: Nav2 Navigator object (ex. BasicNavigator)
+        - x, y: cartesian coordinates for the robot's destination.
+        - yaw_deg: bearing
+        - frame_id: coordinate system (map, odom, base_link). The default is the 'map' frame
+    Output:
+        - pose: a PoseStamped object
+    '''
+    pose = PoseStamped()                                            # time-stamped pose obect
+    pose.header.frame_id = frame_id                                 # set the frame_id
+    pose.header.stamp = navigator.get_clock().now().to_msg()        # get and set current timestamp
+
+    # Assigning the 3D coordinates
     pose.pose.position.x = float(x)
     pose.pose.position.y = float(y)
-    pose.pose.position.z = 0.0
+    pose.pose.position.z = 0.0                                      # robot stays on the ground so this is obviously 0
 
-    yaw = math.radians(yaw_deg)
+    # Using a quaternion to represent the orientation to prvent gimbal lock 
+    # (x, y, z, w)
+    yaw = math.radians(yaw_deg)                                     # deg --> rad
+    # converting from axis-angle to quaternion (axis is the z-axis and angle is yaw)
+    # updating the orientation
     pose.pose.orientation.x = 0.0
     pose.pose.orientation.y = 0.0
     pose.pose.orientation.z = math.sin(yaw / 2.0)
@@ -24,79 +43,40 @@ def make_pose(navigator, x, y, yaw_deg=0.0, frame_id='map'):
     return pose
 
 
-def square(navigator, side=1.0, frame_id='odom'):
-    negative_side = -1 * side
+
+def load_waypoints_from_file(navigator, file_path, frame_id='odom'):
+    '''
+    Loads a list of waypoints from a specififed yaml file
+    Inputs:
+        - navigator:  Nav2 Navigator object (ex. BasicNavigator)
+        - file_path: waypoints.yaml
+        - frame_id: coordinate system (map, odom, base_link). The default is the 'odom' frame
+    '''
+    with open(file_path, 'r') as f:
+        data = yaml.safe_load(f)
+        
+    # for each waypoint defined, create a PoseStamped Object
+    # Put them in an array and return
     return [
-        make_pose(navigator, 0.0, 0.0, 180, frame_id),
-        make_pose(navigator, negative_side, 0.0, 180, frame_id),
-        make_pose(navigator, negative_side, side, 90, frame_id),
-        make_pose(navigator, 0.0, side, 0, frame_id),
-        make_pose(navigator, 0.0, 0.0, -90, frame_id),
+        make_pose(navigator, p['x'], p['y'], p['yaw'], frame_id) 
+        for p in data['waypoints']
     ]
-
-
-def triangle(navigator, side=1.0, frame_id='odom'):
-  #  h = math.sqrt(3) / 2.0 * side
-    return [
-        make_pose(navigator, 0.0, 0.0, 0, frame_id),
-        make_pose(navigator, -2.0, 0.0, 180, frame_id),
-        make_pose(navigator, -1.0, 1, -135, frame_id),
-        make_pose(navigator, 0.0, 0.0, 0.0, frame_id),
-    ]
-
-
-def zigzag(navigator, step=0.75, frame_id='odom'):
-    return [
-        make_pose(navigator, 0.0, 0.0, 0, frame_id),
-        make_pose(navigator, step, step, 0, frame_id),
-        make_pose(navigator, 2 * step, 0.0, 0, frame_id),
-        make_pose(navigator, 3 * step, step, 0, frame_id),
-        make_pose(navigator, 4 * step, 0.0, 0, frame_id),
-    ]
-
-
-def line(navigator, frame_id='odom'):
-    waypoints = []
-
-    waypoints.append(make_pose(navigator, 0.0, 0.0, 0, frame_id))
-    waypoints.append(make_pose(navigator, 1.0, 0.0, 0, frame_id))
-    waypoints.append(make_pose(navigator, 2.0, 0.0, 0, frame_id))
-    return waypoints 
-
-
-def circle(navigator, radius=1.0, num_points=12, frame_id='odom'):
-    poses = []
-    for i in range(num_points):
-        theta = 2.0 * math.pi * i / num_points
-        x = radius * math.cos(theta)
-        y = radius * math.sin(theta)
-        yaw_deg = math.degrees(theta + math.pi / 2.0)  # tangent-ish heading
-        poses.append(make_pose(navigator, x, y, yaw_deg, frame_id))
-    poses.append(make_pose(navigator, radius, 0.0, 90, frame_id))  # close loop
-    return poses
 
 
 def main():
     rclpy.init()
     navigator = BasicNavigator()
-
-    # Wait until Nav2 is active
-    navigator.waitUntilNav2Active(localizer='robot_localization')
-    
-    # Pick one:
-#    waypoints = square(navigator, side=1.0, frame_id='odom')
-    # waypoints = triangle(navigator, side=1.0, frame_id='odom')
-    # waypoints = zigzag(navigator, step=0.75, frame_id='map')
-    # waypoints = circle(navigator, radius=1.0, num_points=16, frame_id='map')
-    waypoints = line(navigator, frame_id='odom')
-    navigator.followWaypoints(waypoints)
+    navigator.waitUntilNav2Active(localizer='robot_localization')                               # Wait until Nav2 is active
+    waypoints = load_waypoints_from_file(navigator, FILE_PATH, frame_id='odom')                 # Load waypoints
+    navigator.followWaypoints(waypoints)                                                        # use Nav2's followWaypoints module to navigate through the specified waypts
 
     while not navigator.isTaskComplete():
-        feedback = navigator.getFeedback()
+        feedback = navigator.getFeedback()                                                  
         if feedback:
             print("Following waypoints...")
 
-    result = navigator.getResult()
+    result = navigator.getResult()                                                              # grab result status 
+
 
     if result == TaskResult.SUCCEEDED:
         print("Waypoints completed successfully.")
@@ -107,7 +87,7 @@ def main():
     else:
         print("Unknown result.")
 
-    rclpy.shutdown()
+    rclpy.shutdown()                                                                            # shut the nodes
 
 
 if __name__ == '__main__':
