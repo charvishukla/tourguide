08b25b22aa5c6b6f3fdade48a1751dca3d05bbe7
Charvi Shukla <cshukla@ucsd.edu>
2026-04-27 23:43:39 +0200

    Create a tourguide bringup package; Brings up the apriltags node

---
+++ src/tourguide_bringup/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+
+package_name = 'tourguide_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Charvi Shukla',
+    maintainer_email='cshukla@ucsd.edu',
+    description='Qualcomm Tourguide Bringup Package',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
